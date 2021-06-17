3524be29bec5f28378dae8a6c97ce14ea965cfba
EugeneKulikovskiy <eugene.kulikovskiy@waveaccess.ru>
2021-06-17 22:41:38 +0300

    edit model changes

---
+++ siteMain/Models/ActorsEdit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using siteMain.Domain.Entities;
+
+namespace siteMain.Models
+{
+    public class ActorsEdit
+    {
+        [Required(ErrorMessage = "Заполните имя актера")]
+        [Display(Name = "Имя актера")]
+        public string Title { get; set; }
+
+        [Display(Name = "Краткая биография актера")]
+        public string Subtitle { get; set; }
+
+        [Display(Name = "Полная биография актера")]
+        public string Text { get; set; }
+
+        [Display(Name = "Средний рейтинг актера")]
+        public float AvgRateActor { get; set; }
+
+        public List<FilmsAndActorsModel> FilmsAndActors { get; set; }
+
+        [Required]
+        public Guid Id { get; set; }
+
+
+        [Display(Name = "Титульная картинка")]
+        public virtual string TitleImagePath { get; set; }
+
+
+        [Display(Name = "SEO метатег Title")]
+        public virtual string Metatitle { get; set; }
+
+
+        [Display(Name = "SEO метатег Description")]
+        public virtual string MetaDescription { get; set; }
+
+
+        [Display(Name = "SEO метатег KeyWords")]
+        public virtual string MetaKeyWords { get; set; }
+
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
